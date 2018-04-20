9aba7d8aacd8c153f3c4574f4bcf45dc3eb8239a
Mohammad Sabri <mohsmh0@gmail.com>
2018-04-20 22:57:55 +0200

    Update crew.js

--- crew.js
+++ crew.js
@@ -1,35 +1,19 @@
 var number = document.getElementById("number").value;
 
 
-var splitedNumbersArray = number.toString().split("");
-
-var firstNumber = splitedNumbersArray[0];
-
-var secondNumber = splitedNumbersArray[1];
-
-var thirdNumber = splitedNumbersArray [2];
-
-var digits = [
-    firstNumber, secondNumber, thirdNumber
-];
+var noOfDigits = number.toString().length;
 
 
-console.log(digits);
-
-
-
-for( i=0; i<= digits.length;  i++ ){
+for( i=0; i< noOfDigits;  i++ ){
 	
-	
-    function isEven(){
-	  if( digits[0]% 2== 0){
-		console.log(digits[0] + " is Even , congratulations !");
-	  }else{
-		console.log(digits[0]+" is not Even Sorry");
-		console.log(digits[0]-1);
-	}
+	function isEven(){
+	    digit=number/Math.pow(10, noOfDigits-1);
+	  if( digit % 2!= 0){
+		console.log(digit + " is not Even , going to work on it !");
+	  }
+	  elseif(i==nOfDigits-1){
 
-
+	  }
 }
 isEven();
 };
